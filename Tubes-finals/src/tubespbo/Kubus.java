0b20da8cc72a058b9efa94bc80099f6928f443bf
ruthhhs <ruthseptrianas@gmail.com>
2026-05-24 01:09:00 +0700

    test pindah ke netbeans

---
+++ Tubes-finals/src/tubespbo/Kubus.java
@@ -0,0 +1,54 @@
+package tubespbo;
+
+public class Kubus extends BangunRuang {
+    // ====== ATRIBUT ======
+    private BujurSangkar sisi;
+
+    // ====== METHOD KONSTRUKTOR ======
+    public Kubus(){
+        setJumlahSisi(6);
+        setJumlahRusuk(12);
+        this.sisi = new BujurSangkar();
+    }
+    
+    public Kubus(BujurSangkar sisi){
+        super(6, 12);
+        this.sisi = sisi;
+    }
+
+    // ====== METHOD SELEKTOR ======
+    public BujurSangkar getSisi(){
+        return sisi;
+    }
+
+    // ====== METHOD MUTATOR  ======
+    public void setSisi(BujurSangkar sisi){
+        this.sisi = sisi;
+    }
+
+    // ====== METHOD LAINNYA ======
+    public double getDiagonalRuang(){
+        return sisi.getSisi() * Math.sqrt(3);
+    }
+
+    @Override
+    public double getLuasPermukaan(){
+        return 6 * Math.pow(sisi.getSisi(), 2);
+    }
+
+    @Override
+    public double getVolume(){
+        return Math.pow(sisi.getSisi(), 3);
+    }
+
+    @Override
+    public String printDetail(){
+        return
+            "Warna         = " + this.getSisi().getWarna() +
+            "\nSisi          = " + this.getSisi().getSisi() +
+            "\nDiagonal Ruang = " + this.getDiagonalRuang() +
+            "\nLuas Permukaan = " + this.getLuasPermukaan() +
+            "\nVolume         = "+ this.getVolume();
+    }
+
+}
